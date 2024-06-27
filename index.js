588903092bf07863b1c2f4e603ad9006c8ede5ad
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-06-27 13:55:08 -0700

    updated the index file based on teh test results

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const inquirer = require("inquirer");
-const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+
 const fs = require("fs");
-const notEmpy = require('./lib/ValidateNotEmpty');
+
 const setShape = require("./lib/generateSVG");
 
-inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
 
 console.log("Welcome to my LOGO maker!!!");
@@ -14,35 +13,31 @@ const questions = [
     type: 'maxlength-input',
     name: 'text',
     message: 'Enter your Brand Initials',
-    maxLength: 3,
-    validate: notEmpy,
+    maxLength: 3    
 }, 
 {
 type: 'input',
 name: 'textColor',
-message: 'Select a color for the Text (Enter a color keyword OR hexadecimal number)',
-validate: notEmpy,
+message: 'Select a color for the Text (Enter a color keyword OR hexadecimal number)'
 
 },
 {
     type: 'list',
     name: 'Shapes',
     message: 'please select a shape for your logo',
-    choices: ['circle', 'triangle', 'square'],
-    validate: notEmpy,
+    choices: ['circle', 'triangle', 'square']    
 
 },
 {
   type: 'input',
   name: 'shapeColor',
-  message: 'Enter the color of the shape:',
-  validate: notEmpy,
+  message: 'Enter the color of the shape:'  
 
 }
 ]
 
 function writeTofile(response) {
-  const filename = `./examples/logo.svg`
+  const filename = `./images/logo.svg`
   const svg = setShape(response);
 
   fs.writeFile(filename, svg, (err) => err ? console.log(err) : console.log("perfect, your logo has been generated"))
