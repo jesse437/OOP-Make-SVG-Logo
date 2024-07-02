d1ffd03bd8897f5d516f174d12b1aec9a9fc63dd
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-07-01 17:03:52 -0700

    Added validation and test files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 
 const fs = require("fs");
-
+const notEmpty = require('./lib/ValidateEmpty');
 const setShape = require("./lib/generateSVG");
 
 
@@ -13,25 +13,28 @@ const questions = [
     type: 'maxlength-input',
     name: 'text',
     message: 'Enter your Brand Initials',
-    maxLength: 3    
+    maxLength: 3,
+    validate: notEmpty,   
 }, 
 {
 type: 'input',
 name: 'textColor',
-message: 'Select a color for the Text (Enter a color keyword OR hexadecimal number)'
-
+message: 'Select a color for the Text (Enter a color keyword OR hexadecimal number)',
+validate: notEmpty,   
 },
 {
     type: 'list',
     name: 'Shapes',
     message: 'please select a shape for your logo',
-    choices: ['circle', 'triangle', 'square']    
+    choices: ['circle', 'triangle', 'square']    ,
+    validate: notEmpty,   
 
 },
 {
   type: 'input',
   name: 'shapeColor',
-  message: 'Enter the color of the shape:'  
+  message: 'Enter the color of the shape:' ,
+  validate: notEmpty,    
 
 }
 ]
